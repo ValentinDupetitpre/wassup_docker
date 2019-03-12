fa6b70910852c901ebe27996e45774fea43773a1
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2019-03-12 16:53:48 +0100

    app as volume done -> step 3 mysql

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello UX Republicain!'))
 
+app.get("/docker", (req, res) => {
+    res.send("hello from docker");
+});
+
+app.get('/nodemon', (req, res) => res.send('hello from nodemon'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
